e51b4db6677606a3af903715ccc231a56ea8ba2c
Kelton Manzanares <kmanzana@mines.edu>
2013-12-09 11:27:23 -0700

    fixed some styles issues and a test

--- spec/bwon_output_spec.rb
+++ spec/bwon_output_spec.rb
@@ -1,16 +1,14 @@
-# rubocop:disable Void
 require 'spec_helper'
-# require 'tempfile'
 
 describe 'BWONOutput' do
   before do
-    raw_data_file.write "Suncor Waste Tracking Log V2,,,,,,,,,,,,\nCustomer: Suncor Energy USA- Denver Refinery,,,,,,,,,,,,\nStart Date: 9/1/2013 12:00:00 AM,,,,,,,,,,,,\nEnd Date: 10/1/2013 12:00:00 AM,,,,,,,,,,,,\nVacuum Truck Company,Shift Report Date,Offload Date,Time,Truck Number,Unit,Source,Material,Volume (BBL),Rough Est Water %,\"To Unit/ Discharge\nPoint\",Driver Name,Ticket\nPSC,09/01/2013,09/01/2013,13:00:00,LV-1,CRUDE DOCKS UNLOADING,CRUDE DOCKS - UNDERGROUND TANK - TANK 101,OILY WATER,60,99%,PLANT 1 - VAC PAD,H. Medina,T-ADE-3CF-5CFB\nPSC,09/02/2013,08/31/2013,07:00:00,LV-7,PLANT 1 - NORTH OF THE FENCELINE,P1 N – Sumps/Wells/Trenches (clean truck),OILY WATER,1.50,99%,PLANT 1 - VAC PAD,D. Schock,T-ADE-78D-B2FC\nPSC,09/02/2013,08/31/2013,08:00:00,LV-7,PLANT 1 - NORTH OF THE FENCELINE,P1 N – Tank 3 (clean truck),SLOP OIL,10,<10%,PLANT 2 - TANK 20,D. Schock,T-ADE-78D-B2FC\n"
+    raw_data_file.write "Suncor Waste Tracking Log V2,,,,,,,,,,,,\nCustomer: Suncor Energy USA- Denver Refinery,,,,,,,,,,,,\nStart Date: 9/1/2013 12:00:00 AM,,,,,,,,,,,,\nEnd Date: 10/1/2013 12:00:00 AM,,,,,,,,,,,,\nVacuum Truck Company,Shift Report Date,Offload Date,Time,Truck Number,Unit,Source,Material,Volume (BBL),Rough Est Water %,\"To Unit/ Discharge\nPoint\",Driver Name,Ticket\nPSC,09/01/2013,09/01/2013,13:00:00,LV-1,CRUDE DOCKS UNLOADING,CRUDE DOCKS - UNDERGROUND TANK - TANK 101,OILY WATER,60,99%,PLANT 1 - VAC PAD,H. Medina,T-ADE-3CF-5CFB\nPSC,09/02/2013,08/31/2013,07:00:00,LV-7,PLANT 1 - NORTH OF THE FENCELINE,P1 N – Sumps/Wells/Trenches (clean truck),OILY WATER,1.50,99%,PLANT 1 - VAC PAD,D. Schock,T-ADE-78D-B2FC\nPSC,09/02/2013,08/31/2013,08:00:00,LV-7,PLANT 1 - NORTH OF THE FENCELINE,P1 N – Tank 3 (clean truck),SLOP OIL,10,<10%,PLANT 2 - TANK 20,D. Schock,T-ADE-78D-B2FC\n" # rubocop:disable LineLength
     raw_data_file.rewind
   end
 
   after do
     raw_data_file.close
-    raw_data_file.unlink    # deletes the temp file
+    raw_data_file.unlink
   end
 
   let(:raw_data_file) { Tempfile.new('raw_data') }
@@ -32,7 +30,7 @@ describe 'BWONOutput' do
     end
 
     it 'should produce the correct output' do
-      bwon_build_output.should == '09/01/2013\n09/02/2013\n09/02/2013\n'
+      bwon_build_output.should == "09/01/2013\n09/02/2013\n09/02/2013\n"
     end
   end
 
@@ -45,4 +43,3 @@ describe 'BWONOutput' do
   end
 
 end
-
--- spec/files_spec.rb
+++ spec/files_spec.rb
@@ -1,15 +1,20 @@
-# rubocop:disable Void
 require 'spec_helper'
 
-describe '' do
+describe 'file operations' do
   describe 'post /upload' do
-    before { BWONOutput.stub(:build) }
-    after { tempfile.close; tempfile.unlink }
-
     let(:tempfile) { Tempfile.new('foo') }
 
+    before { BWONOutput.stub(:build) }
+
+    after do
+      tempfile.close
+      tempfile.unlink
+    end
+
     it 'should receive the file and start a download' do
-      post '/upload', file: { filename: 'example.csv', tempfile: tempfile.path }
+      post '/upload',
+           file: { filename: 'example.csv', tempfile: tempfile.path }
+
       last_response.should be_ok
       last_response.content_type.should == 'application/octet-stream'
     end
@@ -18,7 +23,7 @@ describe '' do
       it 'should redirect to index' do
         post '/upload'
 
-        last_response.status.should == 302
+        last_response.status.should == 302 # rubocop:disable Void
         last_response.should be_redirect
         follow_redirect!
         last_request.url.should == 'http://example.org/'
